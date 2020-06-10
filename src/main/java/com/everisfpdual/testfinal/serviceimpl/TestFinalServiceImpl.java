96e2c1f70fce231142869698f9259f701c5a3139
noelia96-96 <noeliagalindo96@gmail.com>
2020-06-10 19:20:38 +0200

    getExcel
    lista de usuarios, llamada excel

--- src/main/java/com/everisfpdual/testfinal/serviceimpl/TestFinalServiceImpl.java
+++ src/main/java/com/everisfpdual/testfinal/serviceimpl/TestFinalServiceImpl.java
@@ -2,26 +2,19 @@ package com.everisfpdual.testfinal.serviceimpl;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-/*
+
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellStyle;
-import org.apache.poi.ss.usermodel.FillPatternType;
-import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFFont;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-*/
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
-
 import com.everisfpdual.testfinal.domain.Usuario;
 import com.everisfpdual.testfinal.repository.UsuarioRepository;
 import com.everisfpdual.testfinal.service.TestFinalService;
@@ -34,11 +27,38 @@ public class TestFinalServiceImpl implements TestFinalService{
 	@Autowired
 	UsuarioRepository usuarioRepository;
 	
-	public ByteArrayInputStream getExcel() {
+	public ByteArrayInputStream getExcel() throws IOException {
 		
 		//Enunciado: Obtener lista de Usuarios e implementar la llamada al metodo para obtener el excel
+		String[] columns = {"id","email", "firstname", "lastname", "password"};
+		
+		Workbook workbook = new XSSFWorkbook();
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		
+		Sheet sheet = workbook.createSheet(Constant.USUARIOS_SHEET);
+		Row row = sheet.createRow(0);
+		
+		for (int i = 0; i < columns.length; i++) {
+			Cell cell = row.createCell(i);
+			cell.setCellValue(columns[i]);
+		}
+		
 		List<Usuario> usuarios = new ArrayList<>();
-		ByteArrayInputStream inputStreamResource = null;
+		usuarios = usuarioRepository.findAll();
+		int fila = 1;
+		for(Usuario usuario : usuarios) {
+			row = sheet.createRow(fila);
+			row.createCell(0).setCellValue(usuario.getId());
+			row.createCell(1).setCellValue(usuario.getId());
+			row.createCell(2).setCellValue(usuario.getId());
+			row.createCell(3).setCellValue(usuario.getId());
+			row.createCell(4).setCellValue(usuario.getId());
+			
+		}
+		
+		workbook.write(baos);
+		workbook.close();
+		ByteArrayInputStream inputStreamResource = new ByteArrayInputStream(baos.toByteArray());
 		
 		return inputStreamResource;
 	}
